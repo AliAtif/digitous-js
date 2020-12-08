b8e380076a43682b5ac0235d7cf60bec80c8b132
unknown <at07.if@gmail.com>
2020-12-08 14:25:16 +0100

    exercice-2

---
+++ jour02.js
@@ -0,0 +1,36 @@
+// 01 - Number
+var integer = 102;
+var float = 13.9;
+console.log(integer);
+console.log(float);
+
+// 02 - Convert
+var basic = 34;
+var stringified = basic;
+console.log(stringified)
+
+// 03 - Round
+var num = 1.5;
+var rounded = (Math.round(num));
+console.log(rounded);
+
+// 04 - Arithmétique
+var test = 12;
+var bis = 5;
+console.log(test + bis);
+console.log(test - bis);
+console.log(test * bis);
+console.log(test / bis);
+console.log(test % bis);
+console.log(test ** bis);
+
+// 05 - Comparaison
+var test = 143;
+var bis = 219;
+console.log(test > bis);
+console.log(test < bis);
+console.log(test = bis);
+console.log(test == bis);
+console.log(test === bis);
+
+// 06 - Condition
