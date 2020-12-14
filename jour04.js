576f425baee35006260c748552806a9b681adb8a
unknown <at07.if@gmail.com>
2020-12-14 15:38:29 +0100

    exercice-4

--- jour04.js
+++ jour04.js
@@ -26,6 +26,7 @@ cats = Object.assign(cat, cat2);
 console.log(cat.age);
 console.log(cat2.isCute);
 
+
 // 03 - Even
 function checkIfEven(num) {
     if (num % 2 === 0) {
@@ -38,3 +39,31 @@ checkIfEven(100)
 checkIfEven(95)
 checkIfEven(110)
 checkIfEven(213)
+
+
+// 04 - Compare
+function compare(num1, num2) {
+    if (num1 > num2) {
+        console.log("num1 is bigger")
+    } else if (num2 > num1) {
+        console.log("num2 is bigger")
+    } // else(num1 == num2); {
+    //         console.log("both are the same")
+    //     }
+}
+compare(15, 25);
+compare(77, 76);
+compare(18, 18);
+compare(122, 25);
+compare(12, 22);
+
+
+// 05 - Add Up
+function addUp(num) {
+    somme = 0;
+    for (var i = 0; i <= 12; i++) {
+        somme = somme + i
+    }
+    console.log(somme)
+}
+addUp(12)
