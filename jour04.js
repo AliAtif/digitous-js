d63fc3eacbcd4a64c72cb0504508ade0a7860c28
unknown <at07.if@gmail.com>
2020-12-14 14:38:39 +0100

    exercice-4

--- jour04.js
+++ jour04.js
@@ -1,3 +1,4 @@
+// 01 - Object
 var cat = {
     name: "Garfield",
     age: 3,
@@ -9,3 +10,22 @@ console.log(cat.age);
 if (cat.isCute == true) {
     console.log("So cute !");
 }
+
+
+// 02 - Combine
+var cat = {
+    name: "Garfield",
+    age: 3,
+    isCute: true,
+}
+var cat2 = {
+    name: "Mimi",
+    age: 4,
+    isCute: false,
+}
+
+var cats = [cat, cat2];
+cats = Object.assign(cat, cat2);
+
+console.log(cat.age);
+console.log(cat2.isCute);
