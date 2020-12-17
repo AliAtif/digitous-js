9b0fff08f5a5fda1b5be6139e25c96265da2e571
unknown <at07.if@gmail.com>
2020-12-17 22:33:35 +0100

    exercice-5 terminé

--- jour05-exoX.js
+++ jour05-exoX.js
@@ -1,21 +1,100 @@
-// 01 - Calculator
-// function calculate(par1, par2, par3){
-//     if(par2!==0){
-//         return 
-//     }
+// 01 - Calculator (switch)
 
-// }
+// Calculette
+// demander a l'utilisatuer deux nombres entier et un operateur
+
+// number
+// string => "+"
+// boolean
+// array
+// object
+
+function calculate2(number1, number2, operator) {
+    switch (operator) {
+        case "+":
+            {
+                return number1 + number2;
+            }
+        case "-":
+            {
+                return number1 - number2;
+            }
+        case "x":
+            {
+                return number1 * number2;
+            }
+        case "/":
+            {
+                return number1 / number2;
+            }
+        case "%":
+            {
+                return number1 % number2;
+            }
+        default:
+            {
+                console.log(`${operator} is not managed by our calculator`);
+            }
+    }
+}
+
+
+// 01 - Calculator 2 (if)
+
+function calculate(number1, number2, operator) {
+    if (operator === "+") {
+        return number1 + number2;
+    } else if (operator === "-") {
+        return number1 - number2;
+    } else if (operator === "x") {
+        return number1 * number2;
+    } else if (operator === "/") {
+        return number1 / number2;
+    } else if (operator === "%") {
+        return number1 % number2;
+    } else {
+        console.log(`${operator} is not managed by our calculator`);
+    }
+}
+
+console.log(process.argv);
+
+if (process.argv.length !== 5) {
+    console.log("Error: I'm expecting 3 parameters");
+} else {
+    var result = calculate(parseInt(process.argv[2]), parseInt(process.argv[4]), process.argv[3]);
+    console.log(result);
+}
+
+//=======================================================================================================
 
 
 // 02 - Table
-function multiply(num) {
-    console.log(`Table de multiplication du ${num}`);
-    for (let i = 1; i < 11; i = i + 1) {
-        console.log(num + " x " + i + " = " + num * i);
+function multiply(number) {
+    for (var i = 1; i <= 10; i++) {
+        // 5 x 6 = 30
+        console.log(`${number} x ${i} = ${number * i}`);
     }
 }
-console.log(process.argv);
-multiply(process.argv[2]);
+multiply(parseInt(process.argv[2]));
+
+//=================================================================
 
 
 // 03 - Separate Table
+var { multiply, addition } = require("./table-utils.js");
+
+// qu'est ce que je veux afficher ? 
+// une table de multiplication
+// number ? string ? boolean ? array ? object ? null  ? undefined ? 
+
+if (process.argv.length !== 4) {
+    console.log("Error");
+} else {
+    // 6
+    multiply(parseInt(process.argv[2]));
+    //console.log(result.join("\n"));
+
+    // 8
+    addition(parseInt(process.argv[3]));
+}
