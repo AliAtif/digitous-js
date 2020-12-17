5240392eb4c9c8a2addde95686c790904a676d24
unknown <at07.if@gmail.com>
2020-12-17 21:44:31 +0100

    exercice-6

--- jour06.js
+++ jour06.js
@@ -1,22 +1,74 @@
 // 01 - Format
-function formatDate(num, num1, num2) {
-    var format = new Date();
-    var result = format.toDateString(num, num1, num2);
-
-    console.log(result);
+function formatDate(stringDate) {
+    var date = new Date(stringDate);
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
-formatDate()
+console.log(formatDate("2020-12-11"));
 
+// 01 - Format 2 (moment)
+var moment = require("moment");
+
+function formatDate(stringDate) {
+    var date = moment(stringDate);
+    return date.format("DD/MM/YYYY");
+}
+console.log(formatDate("2020-12-11"));
+
+//=========================================================
 
 // 02 - Age
-// function calculateAge(a, b, c) {
-//     var birthday = parseInt(a, b, c);
-//     var today = new Date();
-//     var birthday = new Date();
-//     var age = today - birthday;
-// }
-// result = calculateAge();
-// console.log(result);
+function calculateAge(birthday) {
+    var date = new Date(birthday);
+    var age = new Date(new Date() - date);
+    return age.getFullYear() - 1970;
+}
+console.log(calculateAge("1988-11-17"));
 
 
-// 03 - Validator
+// 02 - Age 2 ()
+function calculateAge(birthday) {
+    var date = moment(birthday);
+    return date.fromNow()
+}
+console.log(calculateAge("1988-11-17"));
+
+//========================================================
+
+
+// 03 - Validator (prompt)
+var prompt = require("prompt");
+
+var schema = {
+    properties: {
+        email: { pattern: /^\S+@\S+\.\S+$/, required: true, message: "Email non valide" },
+        username: { pattern: /^[a-zA-Z-]+$/, required: true, message: "Username non valide" },
+        password: {
+            required: true,
+            message: "Password non valide",
+            conform: function(value) {
+                if (value.search(/.{6,}/) === -1) {
+                    return false;
+                }
+
+                if (value.search(/[a-zA-Z]/) === -1) {
+                    return false;
+                }
+
+                if (value.search(/[0-9]/) === -1) {
+                    return false;
+                }
+
+                if (value.search(/[^a-zA-Z0-9-]/) > -1) {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+    },
+};
+
+prompt.start();
+prompt.get(schema, (err, res) => {
+    console.log(res);
+});
