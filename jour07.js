11b56419be460c5f85fb7f74c9740c5d33167ed9
unknown <at07.if@gmail.com>
2020-12-17 14:33:08 +0100

    exercice vacances

---
+++ jour07.js
@@ -0,0 +1,31 @@
+// ## 01 - Alphabet
+
+function sortLetters(str) {
+    console.log(str);
+    var letters = str.split("");
+    console.log(letters.sort());
+    console.log(letters.sort().join(""));
+
+}
+sortLetters("konexio");
+
+
+
+// 02 - XOXO
+// function countEach(x, o) {
+//     stringed = toString(x, o);
+
+//     if (x.length > o.length) {
+//         console.log(true);
+//     } else {
+//         console.log(false);
+//     }
+// }
+// console.log(countEach("xxxoooox"));
+
+
+// 03 - Palindrome
+function checkPal(st) {
+    st = st.split("");
+
+}
