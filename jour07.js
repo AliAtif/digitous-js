2e7fee253cccee1d30555f2a22599bb8fc542602
unknown <at07.if@gmail.com>
2020-12-17 15:51:44 +0100

    exercices vacances

--- jour07.js
+++ jour07.js
@@ -10,23 +10,48 @@ function sortLetters(str) {
 }
 sortLetters("konexio");
 
+// 01 - Alphapet-2 solution
+// function sortLettersSebastien(word) {
+//     var letters = [];
+//     for (var i = 0; i < word.length; i++) {
+//         letters.push(word[i]);
+//     }
+//     console.log(letters.sort().join(""));
+// }
+
+// sortLetters("konnexio");
+
 
 
 // 02 - XOXO
-// function countEach(x, o) {
-//     stringed = toString(x, o);
+function countEach(word) {
 
-//     if (x.length > o.length) {
-//         console.log(true);
-//     } else {
-//         console.log(false);
-//     }
-// }
-// console.log(countEach("xxxoooox"));
+    var words = word.split("");
+    console.log(words);
+
+    var jointed = words.join(" ");
+    console.log(jointed);
+
+    var x = "";
+    var o = "";
+
+}
+console.log(countEach("xxxoooox"));
 
 
 // 03 - Palindrome
 function checkPal(st) {
-    st = st.split("");
+    sts = st.split("");
+    console.log(sts);
+
+    reversed = sts.reverse();
+    console.log(reversed);
+
+    if (sts == reversed) {
+        console.log("Palindrome !")
+    } else {
+        console.log("Nope")
+    }
 
 }
+checkPal("lit")
