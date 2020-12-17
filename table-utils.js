5824a2fa118057c13c427e932b165808b3fe006a
unknown <at07.if@gmail.com>
2020-12-17 22:30:29 +0100

    exercice-table-utils terminé

--- table-utils.js
+++ table-utils.js
@@ -1,33 +1,22 @@
 // 02 - Table
 
-function multiply(num) {
-    console.log(`Table de multiplication du ${num}`);
-    for (var i = 1; i < 11; i = i + 1) {
-        console.log(num + " x " + i + " = " + num * i);
+function multiply(number) {
+    for (var i = 1; i <= 10; i++) {
+        // 5 x 6 = 30
+        console.log(`${number} x ${i} = ${number * i}`);
     }
-
 }
-console.log(process.argv);
-multiply(process.argv[2]);
 
 // 02 - Table
 
-function addition(num) {
-    console.log(`Table de addition du ${num}`);
-    for (var i = 1; i < 11; i = i + 1) {
-        // console.log(num + i)
-        // result = num + i
-        // return result
-        console.log(num + i);
-
+function addition(number) {
+    for (var i = 1; i <= 10; i++) {
+        // 5 x 6 = 30
+        console.log(`${number} + ${i} = ${number + i}`);
     }
-    // console.log(result);
 }
-console.log(process.argv);
-addition(process.argv[2]);
-// addition(7);
 
 module.exports = {
     multiply,
     addition
-};
+}
