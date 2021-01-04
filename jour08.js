18edc4444cdb3e7663f234db900d28563ee54189
atif <at07.if@gmail.com>
2021-01-04 15:37:32 +0100

    exercice8

--- jour08.js
+++ jour08.js
@@ -12,7 +12,7 @@ fs.readFile("./jour08.txt", function (err, data) {
     console.log("Le contenu  : " + data.toString());
  });
 
- 
+ //--------------------------------------------------------------------------
 
  // façon 2 
  function read(err, data){
@@ -29,12 +29,40 @@ fs.readFile("./jour08.txt", function (err, data) {
 
  // 02 - Map Double
 
- var array =[1, 2, 3, 4, 5];
- var double;
+// façon 1
 
- function doubleArray(array) {
+//  var array =[1, 2, 3, 4, 5];
+//  var double ;
+//  function doubleArray(array) {
+//     return array.map(function(element)  {
+//       return element * 2;
+//     });
+//   } result = doubleArray(array);
+//   console.log(result)
+
+//----------------------------------------------------------
+
+// façon 2 
+
+var array =[1, 2, 3, 4, 5];
+var double = function (array) {
     return array.map(function(element)  {
       return element * 2;
     });
-  } result = doubleArray(array);
-  console.log(result)
+  } 
+  console.log(double(array))
+
+  //=================================================================
+
+  // 03 - Map Names
+
+//   var longNames = [
+// 	{
+// 		firstName: "Jane",
+// 		lastName: "Doe"
+// 	},
+// 	{
+// 		firstName: "John",
+// 		lastName: "Smith"
+// 	}
+// ]
