70b500d011a7652e7d60829674b1f77483fcb317
atif <at07.if@gmail.com>
2021-01-05 16:27:19 +0100

    exercice9

--- jour09.js
+++ jour09.js
@@ -3,7 +3,7 @@
 var request = require('request');
 var countriesNames = [];
 request.get( "https://restcountries.eu/rest/v1/all", function (err, res, body) {
-    var getCountries = JSON.parse(body);
+  var getCountries = JSON.parse(body);
     // console.log(err);
     // console.log(res.statusCode);
     // console.log(body);
@@ -17,3 +17,14 @@ request.get( "https://restcountries.eu/rest/v1/all", function (err, res, body) {
 //================================================================================
 
 // 02 - Chuck Norris
+
+request.get("https://api.chucknorris.io/jokes/random", function(err, res, body){
+    var getFact = JSON.parse(body);
+     getFact.value 
+     console.log( getFact.value );  
+});
+
+
+//=============================================================================================
+
+// 03 - Pokemon
